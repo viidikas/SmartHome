7a58fa1461040c6cefef6461902d8d6f40ba49e7
viidikas <58809000+viidikas@users.noreply.github.com>
2022-01-25 22:48:56 +0200

    some changes to relay board

--- relay.js
+++ relay.js
@@ -1,17 +1,21 @@
 const schedule = require('node-schedule');
 const raspi = require('raspi');
 const I2C = require('raspi-i2c').I2C;
+const Gpio = require('onoff').Gpio;
 const security = require("./security");
 const I2CADDRESS = 0x10;
 const PUMP = 0x01;
 const ESIK = 0x02;
+const KORIDOR = 0X03;
+const ATTIC = 0X04;
+const security = new Gpio(4, 'in', 'both');
 
 const rule = new schedule.RecurrenceRule();
 rule.hour = [0, new schedule.Range(6, 22)];
 
 const i2c = new I2C();
 
-
+security.watch()
 
 /*****Raspberry k2ivitus*****/
 
@@ -19,7 +23,7 @@ raspi.init(() => {
 
     //Pumba taimer
     schedule.scheduleJob(rule, async () => {
-        while (security.armed) {
+        while (listenSecurity) {
             i2c.writeByte(I2CADDRESS, PUMP, 0x10);
             console.log("Pump should be on.")
             // await sleep(180000);
@@ -30,7 +34,6 @@ raspi.init(() => {
             await sleep(2000);
         }
     })
-
 });
 
 function sleep(ms) {
@@ -46,7 +49,12 @@ function turnOnLight(relay, onoff) {
     i2c.writeByte(I2CADDRESS, relay, onoff)
 }
 
+async function listenSecurity() {
+    security.watch((err, value) => {
+        
+        if (value === 1) return true;
+        
+    }).catch((err) => console.log(err.message))
+}
 
-
-
-module.exports = { turnOnLight };
+module.exports = { turnOnLight, listenSecurity };
