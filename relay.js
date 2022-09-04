b14a3e0199dade8ee2fef4be10173c207cb06b8a
Viljar <viljar@viljar.ee>
2022-09-04 23:31:14 +0300

    Added new socket.io stuff

--- relay.js
+++ relay.js
@@ -7,7 +7,7 @@ const PUMP = 0x01;
 const ESIK = 0x02;
 const KORIDOR = 0x03;
 const ATTIC = 0x04;
-const security = new Gpio(4, "in", "both");
+// const security = new Gpio(4, "in", "both");
 
 const i2c = new I2C();
 
@@ -16,33 +16,33 @@ security.watch();
 /*****Raspberry k2ivitus*****/
 raspi.init();
 
-var start = schedule.schedulejob({ hour: 7 }, () => {
-  turnOnAndOff20MinuteCycle;
-});
-var end = schedule.schedulejob({ hour: 22 }, () => {
-  clearTimeout(turnOnAndOff20MinuteCycle);
-});
+// var start = schedule.schedulejob({ hour: 7 }, () => {
+//   turnOnAndOff20MinuteCycle;
+// });
+// var end = schedule.schedulejob({ hour: 22 }, () => {
+//   clearTimeout(turnOnAndOff20MinuteCycle);
+// });
 
-var turnOnAndOff20MinuteCycle = setInterval(pumpDailyCycle, 12000000);
+// var turnOnAndOff20MinuteCycle = setInterval(pumpDailyCycle, 12000000);
 
-function pumpDailyCycle() {
-  pumpTurnOn();
-  var turnOff = setInterval(pumpTurnOff, 1020000);
-}
+// function pumpDailyCycle() {
+//   pumpTurnOn();
+//   var turnOff = setInterval(pumpTurnOff, 1020000);
+// }
 
-function pumpTurnOn() {
-  i2c.writeByte(I2CADDRESS, PUMP, 0x10);
-  console.log("Pump should be on.");
-}
+// function pumpTurnOn() {
+//   i2c.writeByte(I2CADDRESS, PUMP, 0x10);
+//   console.log("Pump should be on.");
+// }
 
-function pumpTurnOff() {
-  i2c.writeByte(I2CADDRESS, PUMP, 0x00);
-  console.log("Pump should be off.");
-}
+// function pumpTurnOff() {
+//   i2c.writeByte(I2CADDRESS, PUMP, 0x00);
+//   console.log("Pump should be off.");
+// }
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+// function sleep(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
 
 function turnOnLight(relay, onoff) {
   if (onoff == 1) {
@@ -53,12 +53,12 @@ function turnOnLight(relay, onoff) {
   i2c.writeByte(I2CADDRESS, relay, onoff);
 }
 
-async function listenSecurity() {
-  security
-    .watch((err, value) => {
-      if (value === 1) return true;
-    })
-    .catch((err) => console.log(err.message));
-}
+// async function listenSecurity() {
+//   security
+//     .watch((err, value) => {
+//       if (value === 1) return true;
+//     })
+//     .catch((err) => console.log(err.message));
+// }
 
-module.exports = { turnOnLight, listenSecurity };
+module.exports = { turnOnLight };
--- webserver.js
+++ webserver.js
@@ -1,59 +1,81 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+var fs = require('fs') //require filesystem module
+var path = require('path')
 
-var http = require('http').createServer(handler); //require http server, and create server with function handler()
-var fs = require('fs'); //require filesystem module
+app.use(express.static(path.join(__dirname, 'public')));
 
-var io = require('socket.io')(http, {
-  allowEIO3: true
+app.get('/', (req, res) => {
+  fs.readFile(__dirname + '/public/index.html', function (err, data) {
+    //read file index.html in public folder
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' }) //display 404 on error
+      return res.end('404 Not Found')
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' }) //write HTML
+    res.write(data) //write data from index.html
+    return res.end()
+  })
 })
 
-var lightvalue = 0;
-
-
-http.listen(8080); //listen to port 8080
-
-
-
-/****  Webserver creation  ******/
-
-function handler(req, res) { //create server
-  fs.readFile(__dirname + '/public/index.html', function (err, data) { //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' }); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
-  });
-}
-
-/******  WebSocket connection  ********/
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('light', lightvalue) //Tell the client the status of the light
-  console.log("New client " + lightvalue)
-
-io.on("connect error", (err) => {
-    console.log(`connect_error due to ${err.message}`);
-  });
+io.on('connection', socket => {
+  var lightvalue = 0;
+  console.log('A user connected')
   
-
-  // this gets called whenever client presses lightswitch
-  socket.on("light", function (data) { 
+  socket.on('light', function(data) {
     lightvalue = data
-    io.emit('light', lightvalue)
+    // socket.emit('light', lightvalue)
+  })
 
-    // if (lightvalue != LED.readSync()) {
-    //   console.log(lightvalue) //only change LED if status has changed
-    //   // LED.writeSync(lightvalue); //turn LED on or off
-    // }
-  });
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+})
 
-});
-
-process.on('SIGINT', function () { //on ctrl+c
-  // i2c.close();
-  process.exit(); //exit completely
-});
+server.listen(3000, () => {
+  console.log('listening on *:3000')
+})
 
 
+// var http = require('http').createServer(handler); //require http server, and create server with function handler()
+// var fs = require('fs'); //require filesystem module
+// var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+// var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+// var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
+// var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
+
+// http.listen(8080); //listen to port 8080
+
+// function handler (req, res) {
+//   //create server
+//   fs.readFile(__dirname + '/public/index.html', function (err, data) {
+//     //read file index.html in public folder
+//     if (err) {
+//       res.writeHead(404, { 'Content-Type': 'text/html' }) //display 404 on error
+//       return res.end('404 Not Found')
+//     }
+//     res.writeHead(200, { 'Content-Type': 'text/html' }) //write HTML
+//     res.write(data) //write data from index.html
+//     return res.end()
+//   })
+// }
+
+// io.sockets.on('connection', function (socket) {// WebSocket Connection
+//   var lightvalue = 0; //static variable for current status
+//     socket.on('light', function(data) { //get light switch status from client
+//       lightvalue = data;
+//         socket.emit('light', lightvalue); //send button status to client
+//     // }
+//   });
+// });
+
+// process.on('SIGINT', function () { //on ctrl+c
+//   LED.writeSync(0); // Turn LED off
+//   LED.unexport(); // Unexport LED GPIO to free resources
+//   pushButton.unexport(); // Unexport Button GPIO to free resources
+//   process.exit(); //exit completely
+// });
